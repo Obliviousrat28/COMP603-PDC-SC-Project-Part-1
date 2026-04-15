76b5e0407de1c022f056b0a8b2c11fd91687c7f0
Tom Williams <tomwillnz@gmail.com>
2026-04-15 21:12:11 +1200

    Started the CUI

--- ServiceDeskProject/src/main/java/com/mycompany/servicedeskproject/ServiceDesk.java
+++ ServiceDeskProject/src/main/java/com/mycompany/servicedeskproject/ServiceDesk.java
@@ -5,12 +5,35 @@
  */
 package com.mycompany.servicedeskproject;
 
+import java.util.Scanner;
+
 /**
  *
  * @author tomwi
  */
 public class ServiceDesk {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        //place holder text for the title
+        System.out.println("Welcome to the software");
+        //next outputs are to example what each input does
+        System.out.println("type 1 to make a request");
+        System.out.println("type 2 to view requests");
+        System.out.println("type 3 to update a request status");
+        System.out.println("type 4 to update a request priority");
+        System.out.println("type 5 to resolve a request");
+        System.out.println("type 0 to exit");
+        
+        //initiate scanner here to allow for user input
+        Scanner scanner = new Scanner(System.in);
+        
+        //put input into a variable to allow switch to work
+        int num = scanner.nextInt();
+        
+        switch(num){
+            case 1:
+                System.out.println("Opening ticket creation");
+                //code to go to a different java file for creation
+                break;
+        }
     }
 }
