19d93aeb19e9776717e1feb22dfe87af76ef6ecb
Tom Williams <tomwillnz@gmail.com>
2026-05-19 00:34:01 +1200

    Started the GUI coding #IMPORTANT READ Code isn't fully operational parts aren't working the GUI works the save file and view and create tickets sometimes don't

--- ServiceDeskProject/src/main/java/com/group51/servicedeskproject/app/ServiceDesk.java
+++ ServiceDeskProject/src/main/java/com/group51/servicedeskproject/app/ServiceDesk.java
@@ -11,18 +11,31 @@ import com.group51.servicedeskproject.repository.FileTicketRepository;
 import com.group51.servicedeskproject.repository.TicketRepository;
 import com.group51.servicedeskproject.service.TicketService;
 import com.group51.servicedeskproject.ui.ConsoleUI;
+import com.group51.servicedeskproject.ui.GUI;
 
 /**
  *
  * @author tomwi
  */
+
+
 public class ServiceDesk {
     public static void main(String[] args) {
-
+        // 1. Keep your working backend logic exactly as it is!
         TicketRepository repository = new FileTicketRepository();
         TicketService ticketService = new TicketService(repository);
 
-        ConsoleUI ui = new ConsoleUI(ticketService);
-        ui.start();
+        // 2. Replace ConsoleUI with your GUI launch wrapper
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                // FIX: Pass the running backend engine right here into the parentheses!
+                GUI startScreen = new GUI(ticketService); 
+
+                // Center it and show it
+                startScreen.setLocationRelativeTo(null);
+                startScreen.setVisible(true);
+            }
+        });
     }
 }
