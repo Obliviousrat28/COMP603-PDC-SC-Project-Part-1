9292b87eb1e2ae4607bee693826942730bd55cc8
Tom Williams <tomwillnz@gmail.com>
2026-06-04 23:41:30 +1200

    user login somewhat done

--- ServiceDeskProject/src/main/java/com/group51/servicedeskproject/repository/DatabaseManager.java
+++ ServiceDeskProject/src/main/java/com/group51/servicedeskproject/repository/DatabaseManager.java
@@ -2,6 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
+package com.group51.servicedeskproject.repository;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
